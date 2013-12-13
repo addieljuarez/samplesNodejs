c2c253729b4f9eefe3b17baec5e81bf96f9c58de
Addiel <addieljuarez@gmail.com>
2013-12-13 17:37:55 -0600

    http init

---
+++ http/server.js
@@ -0,0 +1,6 @@
+var http  = require('http');
+http.createServer(function(request, response){
+	response.writeHead(200, {"Content-Type":"text/html"});
+	// response.write("Hola Mundo");
+	response.end('test');
+}).listen(8888);
